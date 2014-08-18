799a1da7f0f231467ee9a9d33b11f4a87e9dc68d
patrickdavid & Linda Gibson <student@epicodus.com>
2014-08-18 11:12:55 -0700

    Allow users to remove a book from the collection

--- lib/Books.rb
+++ lib/Books.rb
@@ -1,21 +1,41 @@
+require 'pry'
+
 class Books
   attr_accessor :title, :author, :id
 
   def initialize(attributes)
     @title = attributes[:title]
     @author = attributes[:author]
-    @id = attributes[:id]
+    @id = attributes[:id].to_i
   end
 
   def self.all
     entire_selection = []
-    results = DB.exec("SELECT * FROM books")
+    results = DB.exec("SELECT * FROM books;")
+    # binding.pry
     results.each do |result|
-      entire_selection << Books.new(result)
+      title = result['title']
+      author = result['author']
+      id = result['id'].to_i
+      entire_selection << Books.new({:title => title, :author => author, :id => id})
     end
     entire_selection
   end
 
-  # def add
-  #   DB.exec("INSERT INTO books (title, author) VALUES ('#{title}', '#{author}');")
+  def save
+    results = DB.exec("INSERT INTO books (title, author) VALUES ('#{@title}', '#{@author}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def self.delete(input_title)
+    Books.all.each do |book|
+      if input_title == book
+        DB.exec("DELETE FROM books WHERE id = #{input_title.id};")
+      end
+    end
+  end
+
+  def ==(another_book)
+    self.title == another_book.title && self.author == another_book.author
+  end
 end
--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -6,7 +6,22 @@ describe 'Books' do
     expect(test_book).to be_an_instance_of Books
   end
 
-  it 'strats out as an empty library' do
+  it 'starts out as an empty library' do
     expect(Books.all).to eq []
+  end
+
+  it 'adds a book into the books table' do
+    test_book = Books.new({:title => "The Giving Tree", :author => 'Shel Silverstein'})
+    test_book.save
+    expect(Books.all).to eq [test_book]
+  end
+
+  it 'deletes a book from the books table' do
+    test_book = Books.new({:title => "The Giving Tree", :author => 'Shel Silverstein'})
+    test_book.save
+    test_book1 = Books.new({:title => "The Notebook", :author => 'Nicholas Sparks'})
+    test_book1.save
+    Books.delete(test_book)
+    expect(Books.all).to eq [test_book1]
   end
 end
