9b51aa1e0ec1278d5cfecf24ee7be3278b6a22c0
patrickdavid & Linda Gibson <student@epicodus.com>
2014-08-18 13:48:49 -0700

    user adds an author to the authors table.

--- lib/author.rb
+++ lib/author.rb
@@ -5,4 +5,25 @@ class Author
     @name = attributes[:name]
     @id = attributes[:id].to_i
   end
+
+  def self.all
+    entire_authors = []
+    results = DB.exec("SELECT * FROM authors;")
+    # binding.pry
+    results.each do |result|
+      name = result['name']
+      id = result['id'].to_i
+      entire_authors << Author.new({:name => name, :id => id})
+    end
+    entire_authors
+  end
+
+  def save
+    results = DB.exec("INSERT INTO authors (name) VALUES ('#{@name}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def ==(another_author)
+    self.name == another_author.name
+  end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -5,4 +5,14 @@ describe 'Author' do
     test_author = Author.new({:name => 'Shel Silverstein'})
     expect(test_author).to be_an_instance_of Author
   end
+
+  it 'starts as an empty array' do
+    expect(Author.all).to eq []
+  end
+
+  it 'adds an author into the authors table' do
+    test_author = Author.new({:name => 'Shel Silverstein'})
+    test_author.save
+    expect(Author.all).to eq [test_author]
+  end
 end
