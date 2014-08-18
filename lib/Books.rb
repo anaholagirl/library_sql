788c6c83e4f48d457a506dca2d27098491270c93
patrickdavid & Linda Gibson <student@epicodus.com>
2014-08-18 13:31:10 -0700

    User can search library database for books by title

--- lib/Books.rb
+++ lib/Books.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 class Books
-  attr_accessor :title, :author, :id
+  attr_accessor :name, :author, :id
 
   def initialize(attributes)
-    @title = attributes[:title]
+    @name = attributes[:name]
     @author = attributes[:author]
     @id = attributes[:id].to_i
   end
@@ -14,28 +14,45 @@ class Books
     results = DB.exec("SELECT * FROM books;")
     # binding.pry
     results.each do |result|
-      title = result['title']
+      name = result['name']
       author = result['author']
       id = result['id'].to_i
-      entire_selection << Books.new({:title => title, :author => author, :id => id})
+      entire_selection << Books.new({:name => name, :author => author, :id => id})
     end
     entire_selection
   end
 
-  def save
-    results = DB.exec("INSERT INTO books (title, author) VALUES ('#{@title}', '#{@author}') RETURNING id;")
-    @id = results.first['id'].to_i
-  end
-
-  def self.delete(input_title)
+  def self.delete(input_name)
     Books.all.each do |book|
-      if input_title == book
-        DB.exec("DELETE FROM books WHERE id = #{input_title.id};")
+      if input_name == book
+        DB.exec("DELETE FROM books WHERE id = #{input_name.id};")
       end
     end
   end
 
+  def self.find_by_name(input_book)
+    book_arr = []
+    Books.all.each do |book|
+      if input_book == book.name
+        results = DB.exec("SELECT * FROM books WHERE name = '#{input_book}';")
+        results.each do |result|
+          book_arr << result['name']
+        end
+      end
+    end
+    # name = book['name']
+    # author = book['author']
+    # id = book['id'].to_i
+    book_arr
+  end
+
+  def save
+    results = DB.exec("INSERT INTO books (name, author) VALUES ('#{@name}', '#{@author}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+
   def ==(another_book)
-    self.title == another_book.title && self.author == another_book.author
+    self.name == another_book.name && self.author == another_book.author
   end
 end
--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Books' do
   it 'Initializes the Books class' do
-    test_book = Books.new({:title => "The Giving Tree", :author => 'Shel Silverstein'})
+    test_book = Books.new({:name => "The Giving Tree", :author => 'Shel Silverstein'})
     expect(test_book).to be_an_instance_of Books
   end
 
@@ -11,17 +11,26 @@ describe 'Books' do
   end
 
   it 'adds a book into the books table' do
-    test_book = Books.new({:title => "The Giving Tree", :author => 'Shel Silverstein'})
+    test_book = Books.new({:name => "The Giving Tree", :author => 'Shel Silverstein'})
     test_book.save
     expect(Books.all).to eq [test_book]
   end
 
   it 'deletes a book from the books table' do
-    test_book = Books.new({:title => "The Giving Tree", :author => 'Shel Silverstein'})
+    test_book = Books.new({:name => "The Giving Tree", :author => 'Shel Silverstein'})
     test_book.save
-    test_book1 = Books.new({:title => "The Notebook", :author => 'Nicholas Sparks'})
+    test_book1 = Books.new({:name => "The Notebook", :author => 'Nicholas Sparks'})
     test_book1.save
     Books.delete(test_book)
     expect(Books.all).to eq [test_book1]
+  end
+
+  it 'finds a book from the books table with a specific name' do
+    test_book = Books.new({:name => "The Giving Tree", :author => 'Shel Silverstein'})
+    test_book.save
+    test_book1 = Books.new({:name => "The Notebook", :author => 'Nicholas Sparks'})
+    test_book1.save
+    # Books.find_by_name(test_book.name)
+    expect(Books.find_by_name(test_book.name)).to eq [test_book.name]
   end
 end
